17088cdce727b48f3437524124e5fd0b580c9356
enockkipkoech19@gmail.com <enockkipkoech19@gmail.com>
2022-08-03 13:23:57 +0300

    :fire: Implementing Solidity, flashloan, web3 and Hardhat :rocket:

---
+++ contracts/Basics101.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GLP-3.0
+
+pragma solidity ^0.8.0;
+
+contract Basics101 {
+    // General Variables
+    uint256 public myBalance = 30; //positive integer
+    int256 private txAmount = -4; // can be -ve or +ve // can only be called from the contract .
+    string internal coinName = "Epic Coin"; // can only be called by the contract nd other contracts within it.
+    bool isValid = true; // boolean
+
+    // Global Variables
+    uint256 public blockTime = block.timestamp;
+    address public sender = msg.sender;
+
+    // Arrays
+    string[] public tokenNames = ["ChainLink", "Polygon", "DodgeCoin"];
+    uint256[5] levels = [23, 45, 9, 10, 4];
+
+    // Datetime
+    uint256 public timeNow1Sec = 1 seconds;
+    uint256 public timeNow1Min = 1 minutes;
+    uint256 public timeNOw1hr = 1 hours;
+    uint256 public timeNow1Day = 1 days;
+    uint256 public timeNow1week = 1 weeks;
+
+    // Struct
+    struct User {
+        address userAddress;
+        string userName;
+        bool hasTraded;
+    }
+
+    // Store a struct in an array
+    User[] public users;
+}
---
+++ contracts/TestContract.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract TestContract {
+    uint256 public cleanMoney = 10000;
+}
